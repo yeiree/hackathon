87df6ed5b3fd0527de80bd6022c82804ad0894ea
yeiree <yeiree@gmail.com>
2017-10-24 11:36:48 +0900

    itemdata

---
+++ smilecoupon/app/data/raw/itemdata.js
@@ -0,0 +1,39 @@
+const itemdata = [
+    {
+        starbucks: [
+            {
+                no: 1113470217,
+                name: '(스타벅스) 콜드 브루 Tall',
+                price: 4500
+            },
+            {
+                no: 1113470042,
+                name: '(스타벅스) 콜드 브루 라떼 Tall',
+                price: 5000
+            }],
+        coffeebean: [
+            {
+                no: 979852859,
+                name: '(커피빈) 아이스 모카 캐러멜 라떼(S)',
+                price: 4500
+            },
+            {
+                no: 979844768,
+                name: '(커피빈) 캐러멜 아이스 블렌디드(S)',
+                price: 4500
+            }],
+        dunkin: [
+            {
+                no: 1136715688,
+                name: '(던킨도너츠) 치킨 머쉬룸 플랫브레드 콤보',
+                price: 4500
+            },
+            {
+                no: 1136715550,
+                name: '(던킨도너츠) 치킨 머쉬룸 플랫브레드',
+                price: 4500
+            }]
+    }
+];
+
+export default itemdata
